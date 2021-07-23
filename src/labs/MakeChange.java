1e79745ed5ab1cf468cb4cc686771d657cda4ae4
Will Slaunwhite <williamslaunwhite@gmail.com>
2021-07-23 16:16:40 -0600

    added prompt and tendered classes

--- src/labs/MakeChange.java
+++ src/labs/MakeChange.java
@@ -1,5 +1,24 @@
 package labs;
 
-public class MakeChange {
+import java.util.Scanner;
 
+public class MakeChange {
+	public static void main(String[] args) {
+		System.out.println("Hello");
+	}
+
+
+	public static double promptUser() {
+		Scanner sc = new Scanner(System.in);
+		System.out.println("What was the total?");
+		double total = sc.nextDouble();
+		return total;
+	}
+
+	public static double tenderedAmount() {
+		Scanner sc = new Scanner(System.in);
+		System.out.println("How much did the customer pay?");
+		double total = sc.nextDouble();
+		return total;
+	}
 }
