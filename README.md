0ab5ba1c36a203991e252b168dc6879f4a77f099
Will Slaunwhite <williamslaunwhite@gmail.com>
2021-07-25 16:43:22 -0600

    Commiting first working version

--- README.md
+++ README.md
@@ -2,16 +2,43 @@
 
 ### Overview
 
-explain basic idea of project
+Much of how the code works will be explained again inside the code itself, but
+I wanted to include it inside the README as well.
+
+I used main to call three different methods, returnDiff, customerTotal, and
+tenderedAmount. returnDiff is the majority of the class, with a while loop that
+checks if the amount passed to it is greater than 0. it then uses if else
+statements to check if we can return each denomination. if we can return that
+denomination, it passes calculateAmount, which takes the current amount as a
+double and the denomination as either a double or and integer, to cover both
+dollar and cent denominations.
+
+Unfortunately, I had to use DecimalFormat to round my difference to two decimal
+points. I couldn't think of a clever way to fix the issues I was dealing with
+in regards to the length of double decimals and trying to return a consistent
+count with cent denominations. So I created an object of DecimalFormat passing the
+format of two decimal points and used the method format to round at the start of the
+loop and then after every cent denomination.
+
+If you do not want me using this method, let me know and I'll come up with a
+different solution.
+
+High highs and low lows for sure on this project. At some point on Saturday afternoon,
+I thought I had it all put together, but then the code started to fall apart
+when testing the provided cases and I had to rework it until I got to where it's at,
+which was not a short process. But the satisfaction when it ran and passed... priceless.
 
 ### How to use
 
-gives instructions on how to properly run
+It will prompt you for a total and an amount paid. It will then provide the
+denominations of change up to $20 bills.
 
-### Technologies
-
-Conditionals
-Modulus Operator
 
 ### Lessons Learned
-takeaways from project
+I know there's a ton of different ways to solve this problem and I only barely
+solved it one. It's interesting to consider that my classmates almost definitely
+solved this a different way than me and that I can't right now consider what that
+solution might be. But once I see their logic, I'll clearly be able to understand
+how you would build this project that way. That's one of the things I enjoy about
+coding; even though you may have solved a problem there's usually a better way to
+solve it and things to learn from that. 
--- src/labs/MakeChange.java
+++ src/labs/MakeChange.java
@@ -1,143 +1,150 @@
 package labs;
 
+import java.text.DecimalFormat;
 import java.util.Scanner;
+
+		// main here calls three methods, returnDiff, which is the core of the class, customerTotal, which 
+		// creates a scanner and prompts the user for the total and returns that as a double, and tenderedAmount,
+		// which does the same for the amount paid. These doubles are passed to returnDiff, which finds the difference
+		// of the two numbers, then loops through the different denominations while the difference is greater than 0. 
+		// else if statements check to see if we can return the current denomination. If the can return the denomination, 
+		// the call calculateAmount, which is a method that accepts two doubles, an amount and a denomination, and returns
+		// an updated total.
 
 public class MakeChange {
 	public static void main(String[] args) {
 		returnDiff(customerTotal(), tenderedAmount());
-//		calculateChange(x);
 	}
 
+
+		// so, here's what happened. My code was working for the most part but was having some issues 
+		// handling decimal points. And I couldn't really find a way that I could think of to easily
+		// reformat the number, and I couldn't find a solution without rewriting all my code, so I 
+		// looked into the issue and found I could either use BigDecimal or DecimalFormat, and DecimalFormat
+		// seemed easier so I used that to round my variables.  
+		// If you don't want me to use that method, let me know and I'll come up with a different solution. 
+		
+
+		
+		// As stated above, I create a double equaling the difference between what the amount paid was and what 
+		// the actual total was. I then round it to two decimal points. 
 
 	public static void returnDiff(double custTotal, double amountPaid) {
-		double difference = amountPaid - custTotal;
+		double difference = amountPaid-custTotal;
+		DecimalFormat df = new DecimalFormat("#.##");
+		difference = Double.valueOf(df.format(difference));
+		
+		if(difference==0) {
+			System.out.println("Exact change.");
+		}
 		while(difference>0) {
-			System.out.println("Difference 1: "+difference);
-//			System.out.println("Difference 1: "+difference);
-//			System.out.println("Difference 1: "+difference);
-//			System.out.println("Difference 1: "+difference);
 			
 			if(difference-20>0) {
-				System.out.println("Twenties: "+((int)difference/20));
-				difference = calculateDollars(difference, 20);
-				System.out.println("Difference 2: "+difference);
+				System.out.print("Twenties: "+((int)difference/20));
+				difference = calculateAmount(difference, 20);
+				continue;
+				
 			}
-			if(difference-10>0) {
-				System.out.println("Tens: "+((int)difference/10));
-				difference = calculateDollars(difference, 10);
-				System.out.println("Difference 3: "+difference);
+			
+			else if(difference-10>0) {
+				System.out.print(" Tens: "+((int)difference/10));
+				difference = calculateAmount(difference, 10);
+				continue;
 			}
-			if(difference-5>0) {
-				System.out.println("Fives: "+((int)difference/5));
-				difference = calculateDollars(difference, 5);
-				System.out.println("Difference 4: "+difference);
+			
+			else if(difference-5>0) {
+				System.out.print(" Fives: "+((int)difference/5));
+				difference = calculateAmount(difference, 5);
+				continue;
 			}
-			if(difference-1>0) {
-				System.out.println("Ones: "+((int)difference/1));
-				difference = calculateDollars(difference, 1);
-				System.out.println("Difference 5: "+difference);
+			
+			else if(difference-1>0) {
+				System.out.print(" Ones: "+((int)difference/1));
+				difference = calculateAmount(difference, 1);
+				difference = Double.valueOf(df.format(difference));
+				continue;
 			}
-			if(difference-.25>0) {
-				System.out.println("Quarters: "+((int)difference/.25));
-				difference -= calculateCents(difference, .25);
+			
+			else if(difference-.25>0) {
+				System.out.print(" Quarters: "+(int)(difference/.25));
+				difference = calculateAmount(difference, .25);
+				difference = Double.valueOf(df.format(difference));
+				continue;
 			}
-			if(difference-.1>0) {
-				System.out.println("Dimes: "+((int)difference/.1));
-				difference -= calculateCents(difference, .1);
+			
+			else if(difference-.1>0) {
+				System.out.print(" Dimes: "+(int)(difference/.1));
+				difference = calculateAmount(difference, .1);
+				difference = Double.valueOf(df.format(difference));
+				continue;
 			}
-			if(difference-.05>0) {
-				System.out.println("Nickels: "+((int)difference/.05));
-				difference -= calculateCents(difference, .05);
+			
+			else if(difference-.05>0) {
+				System.out.print(" Nickels: "+(int)(difference/.05));
+				difference = calculateAmount(difference, .05);
+				difference = Double.valueOf(df.format(difference));
+				continue;
 			}
-			if(difference-.01>0) {
-				System.out.println("Pennies: "+((int)difference/.01));
-				difference -= calculateCents(difference, .01);
+			
+			else if(difference-.01>=0) {
+				System.out.print(" Pennies: "+(int)(difference/.01));
+				difference = calculateAmount(difference, .01);
+				continue;
+				
 			}
 			break;
-			
-			
-			
-			
-			
-			
-			
-			
-			
-			
+		}	
+		
+		
+		if(difference<0) {
+			System.out.println("Invalid input, please try again...");
 		}
 	}
-			
-
-	public static double calculateDollars(double custTotal, int denomination) {
-		//calculate change
-		//Currently, this function works by passing in the amount FINISH THIS COMMENT
+	
+	
+		// works by taking in the total and denomination, counting how many we could return,
+		// and the adjusting the total by that amount of that denomination. using method 
+		// overload we can cover both dollar denominations and cent denominations. 
+	
+	public static double calculateAmount(double custTotal, int denomination) {
 
 		int counter;
 		counter= (int)(custTotal/denomination);
 		
-//		System.out.print(denomination+": "+counter+" ");
 		custTotal -= (counter*denomination);
 		return(custTotal);
 	}
 	
-	
+	public static double calculateAmount(double custTotal, double denomination) {
 
-	public static double calculateCents(double custTotal, double denomination) {
 		int counter;
-		counter = (int) (custTotal / denomination);
+		counter= (int)(custTotal/denomination);
 		
-//		System.out.print(denomination + ": " + counter + " ");
-		custTotal -= (counter * denomination);
-
-		return (custTotal);
+		custTotal -= (counter*denomination);
+		return(custTotal);
 	}
 	
+		// this method takes input asking what the total was
 	
+		// additionally, i have my scanner close statements commented out because I get some strange
+		// errors when I put them in. Why is this? I think i'm missing something simple here.
 	
-//		this method takes input asking what the total was
 	public static double customerTotal() {
 		Scanner sc = new Scanner(System.in);
 		System.out.println("What was the total?");
 		double total = sc.nextDouble();
-//		sc.close();
+		//sc.close();
 		return total;
 	}
 	
 	
-// 		this method takes input asking how much cust paid
+		// this method takes input asking how much customer paid
 	public static double tenderedAmount() {
 		Scanner sc = new Scanner(System.in);
 		System.out.println("How much did the customer pay?");
 		double total = sc.nextDouble();
-//		sc.close();
+		//sc.close();
 		return total;
 	}
 	
 }
-
-
-
-//if(custTotal>1) {
-//counter= (int)(custTotal/denomination);
-//System.out.print(denomination+": "+counter+" ");
-//custTotal -= (counter*denomination);
-//dolIndex+=1;
-//calculateDollars(custTotal, diffDenomsDollars[dolIndex]);
-//} 
-//
-//else if(custTotal>0 && custTotal<1) {
-//counter= (int)(custTotal/denomination);
-//System.out.print(denomination+": "+counter+" ");
-//custTotal -= (counter*denomination);
-//centIndex+=1;
-//calculateDollars(custTotal, diffDenomsCents[centIndex]);
-
-//from calcDollars
-//	else if(custTotal>=1) {
-//		return custTotal;
-//	} else {
-//		counter = (int)(custTotal/changeDenom);
-//		System.out.println(changeDenom+": "+counter+" ");
-//		custTotal-= (counter*changeDenom);
-//	}
-
